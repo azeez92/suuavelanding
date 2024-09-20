8c3a88b280024f9ac3e005069d1e17250699ba31
Azeez Sodiq <azeezsodiq18@gmail.com>
2024-09-20 13:56:26 +0100

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/suuavelanding/',  // Replace 'suuave' with your actual repository name
+  base: '/suuavelanding/',  // Replace 'suuave' with your actual repository name
 });
